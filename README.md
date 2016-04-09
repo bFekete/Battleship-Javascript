a4f081e6aa9427dac94f042455d4fc1dd99302f4
Brian Fekete <bjf73558@huskies.bloomu.edu>
2016-04-08 22:28:54 -0400

    Added document and debug

---
+++ README.md
@@ -0,0 +1,18 @@
+# Battleship
+
+How to get this project
+
+  - git clone https://github.com/bFekete/Battleship-Javascript.git
+   
+  Have changes?
+
+  - git add *
+  - git commit -m "Added so and so feature"
+  - git push origin master
+
+### Development
+
+Install Firebug extension for Firefox. Press F12 -> Console
+
+
+
--- public_html/js/battleship.js
+++ public_html/js/battleship.js
@@ -1,15 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * battleship.js
+ *  
  */
 var canvas, context;
 var squareHeight, squareWidth;
 
-var ship = {
-    
-}
-
+/**
+ * This function initializes the game and its variables.
+ * @returns {undefined}
+ */
 function start() {
     var container = document.getElementById("gameContainer");
 
@@ -26,19 +25,28 @@ function start() {
     container.appendChild(canvas);
 }
 
+/**
+ * This function is the click event listener for the canvas.
+ * @param {type} e
+ * @returns {undefined}
+ */
 function handleClick(e) {
     // Since in Firefox e.offsetX/Y is undefined...
     xpos = e.offsetX === undefined ? e.layerX : e.offsetX;
     ypos = e.offsetY === undefined ? e.layerY : e.offsetY;
+    console.log("Canvas Click: (" + xpos + ", " + ypos + ")");
 
-    //document.write("<h1>"+canvas.getBoundingClientRect().top+"</h1>");
-    xCoord = Math.floor((xpos - canvas.getBoundingClientRect().left) / 50) * 50;
-    yCoord = Math.floor((ypos - canvas.getBoundingClientRect().top) / 50) * 50;
+    xCoord = Math.floor((xpos - canvas.getBoundingClientRect().left) / squareWidth) * squareWidth;
+    yCoord = Math.floor((ypos - canvas.getBoundingClientRect().top) / squareHeight) * squareHeight;
     context.fillStyle = "red";
     context.fillRect(xCoord, yCoord, squareWidth, squareHeight);
     context.strokeRect(xCoord, yCoord, squareWidth, squareHeight);
 }
 
+/**
+ * This function initializes the grid.
+ * @returns {undefined}
+ */
 function initializeGrid() {
     for (var i = 0; i < 10; i++) {
         for (var k = 0; k < 10; k++) {
@@ -49,6 +57,7 @@ function initializeGrid() {
         }
     }
 }
+
 window.addEventListener("load", start, false);
 
 
