cc16a2d1201f06c126c095364093cde54012e879
Brian Fekete <bjf73558@huskies.bloomu.edu>
2016-04-11 19:23:37 -0400

    Still work in progress

--- public_html/js/battleship.js
+++ public_html/js/battleship.js
@@ -4,6 +4,13 @@
  */
 var canvas, context;
 var squareHeight, squareWidth;
+var radioButton;
+
+function ship(name, size) {
+    this.name = name;
+    this.size = size;
+    this.sank = false;
+}
 
 /**
  * This function initializes the game and its variables.
@@ -11,18 +18,72 @@ var squareHeight, squareWidth;
  */
 function start() {
     var container = document.getElementById("gameContainer");
+    radioButton = document.getElementsByName("ship");
 
     canvas = document.createElement("canvas");
     canvas.addEventListener('click', handleClick);
+    canvas.addEventListener('mousemove', handleMove);
     context = canvas.getContext("2d");
 
     canvas.height = 500;
-    canvas.width = 500;
-    squareHeight = canvas.height / 10;
-    squareWidth = canvas.width / 10;
+    canvas.width = 1050;
+    squareHeight = 50;
+    squareWidth = 50;
     initializeGrid();
 
     container.appendChild(canvas);
+}
+
+
+function handleMove(e) {
+    // Since in Firefox e.offsetX/Y is undefined...
+    xpos = e.offsetX === undefined ? e.layerX : e.offsetX;
+    ypos = e.offsetY === undefined ? e.layerY : e.offsetY;
+    console.log("Canvas Move: (" + xpos + ", " + ypos + ")");
+
+    xCoord = Math.floor((xpos - canvas.getBoundingClientRect().left) / squareWidth) * squareWidth;
+    yCoord = Math.floor((ypos - canvas.getBoundingClientRect().top) / squareHeight) * squareHeight;
+
+    // Carrier 5
+    var carrier = new ship("Carrier", 5);
+    // Battleship 4
+    var battleship = new ship("Battleship", 4);
+    // Submarine 3
+    var submarine = new ship("Submarine", 3);
+    // Cruiser 3
+    var cruiser = new ship("Cruiser", 3);
+    // Patrol 2
+    var patrol = new ship("Patrol", 2);
+
+    // Update the multple squares on the grid when ship is not placed
+    // and the radio button is selected
+    for (var i = 0; i < radioButton.length; i++) {
+        if (radioButton[i].checked) {
+            if (radioButton[i].value === "Battleship") {
+                for (var k = 0; k < battleship.size; k++) {
+                    updateGrid("gray", xCoord + (k * squareWidth), yCoord);
+                }
+            } else if (radioButton[i].value === "Carrier") {
+                for (var k = 0; k < carrier.size; k++) {
+                    updateGrid("gray", xCoord + (k * squareWidth), yCoord);
+                }
+            } else if (radioButton[i].value === "Cruiser") {
+                for (var k = 0; k < cruiser.size; k++) {
+                    updateGrid("gray", xCoord + (k * squareWidth), yCoord);
+                }
+            } else if (radioButton[i].value === "Patrol") {
+                for (var k = 0; k < patrol.size; k++) {
+                    updateGrid("gray", xCoord + (k * squareWidth), yCoord);
+                }
+            } else if (radioButton[i].value === "Submarine") {
+                for (var k = 0; k < submarine.size; k++) {
+                    updateGrid("gray", xCoord + (k * squareWidth), yCoord);
+                }
+            }
+        }
+
+    }
+
 }
 
 /**
@@ -39,6 +100,23 @@ function handleClick(e) {
     xCoord = Math.floor((xpos - canvas.getBoundingClientRect().left) / squareWidth) * squareWidth;
     yCoord = Math.floor((ypos - canvas.getBoundingClientRect().top) / squareHeight) * squareHeight;
     context.fillStyle = "red";
+
+    if (xCoord !== 500) { // So it doesn't set the seperator between the boards
+        context.fillRect(xCoord, yCoord, squareWidth, squareHeight);
+        context.strokeRect(xCoord, yCoord, squareWidth, squareHeight);
+    }
+
+    for (var i = 0; i < radioButton.length; i++) {
+        if (radioButton[i].checked) {
+            radioButton[i].checked = false;
+            radioButton[i].disabled = true;
+        }
+    }
+}
+
+function updateGrid(color, xCoord, yCoord) {
+    context.fillStyle = color;
+    context.strokeStyle = "black";
     context.fillRect(xCoord, yCoord, squareWidth, squareHeight);
     context.strokeRect(xCoord, yCoord, squareWidth, squareHeight);
 }
@@ -54,7 +132,16 @@ function initializeGrid() {
             context.strokeStyle = "black";
             context.fillRect(i * squareWidth, k * squareHeight, squareWidth, squareHeight);
             context.strokeRect(i * squareWidth, k * squareHeight, squareWidth, squareHeight);
+
+            context.fillStyle = "blue";
+            context.strokeStyle = "black";
+            context.fillRect(i * squareWidth + 550, k * squareHeight, squareWidth, squareHeight);
+            context.strokeRect(i * squareWidth + 550, k * squareHeight, squareWidth, squareHeight);
         }
+    }
+
+    for (var i = 0; i < radioButton.length; i++) {
+        radioButton[i].disabled = false;
     }
 }
 
